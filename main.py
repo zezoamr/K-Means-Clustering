36fe139e9d79a583545033d08186f5db381f6df4
ziad amr shalaby <zezoamr@yandex.com>
2023-09-26 12:24:12 +0300

    add a kmeans class which runs k means clustering up to n, figuring out the best number of clusters by comparing the cluster variance for each clustering attempt

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from sklearn.datasets import make_blobs
 
 class KMeansClustering():
     
-    def __init__(self, X, number_of_clusters, max_iterations):
+    def __init__(self, X, number_of_clusters, max_iterations, plot_figure):
         self.k = number_of_clusters
         self.max_iterations = max_iterations
-        self.plot_figure = True
+        self.plot_figure = plot_figure
         self.number_of_examples, self.number_of_features = X.shape
         
     def initialize_centroids(self, X):
@@ -55,15 +55,50 @@ class KMeansClustering():
             self.plot_fig(X, y_pred)
         return y_pred
     
+class KMeans():
+    def __init__(self, X, max_number_of_clusters, max_iterations):
+        self.max_number_of_clusters = max_number_of_clusters
+        self.kmeans_arr = [KMeansClustering(X, i, max_iterations, False) for i in range(1, max_number_of_clusters+1)]
+        self.max_iterations = max_iterations
+    
+    def calculate_variance(self, X, clusters):
+        variance = [0 for _ in range(self.max_number_of_clusters)]
+        for idx, cluster in enumerate(clusters):
+            variance[idx] = np.var(X[cluster], axis=0).sum()
+        return variance
+            
+    def compare_variance(self, iteration_variance):
+        # Find the configuration with the lowest total variance
+        best_variance, best_k = min(iteration_variance, key=lambda x: np.sum(x[0]))
+        # Return the clusters and k value of the best configuration
+        return best_k
+    
+    def plot_fig(self, X, y_pred):
+        plt.scatter(X[:, 0], X[:, 1], c=y_pred, s=40, cmap=plt.cm.Spectral)
+        plt.show()
+    
+    def fit(self, X):
+        variance_arr = []
+        y_pred_arr = []
+        for idx, kmeans in enumerate(self.kmeans_arr):
+            y_pred = kmeans.fit(X)
+            y_pred_arr.append(y_pred)
+            clusters = kmeans.create_clusters(X, kmeans.initialize_centroids(X))
+            variance_arr.append([self.calculate_variance(X, clusters), idx])
+        best_k = self.compare_variance(variance_arr)
+        return y_pred_arr[best_k], best_k + 1
     
 def main():
     np.random.seed(10)
     blob_num_clusters = 5
-    k = 2
+    k = 5
     X, _ = make_blobs(n_samples=1000, n_features=2, centers=blob_num_clusters)
 
-    Kmeans = KMeansClustering(X, k, 100)
-    y_pred = Kmeans.fit(X)
+    #Kmeans = KMeansClustering(X, k, 100, True)
+    Kmeans = KMeans(X, k, 100)
+    y_pred, k = Kmeans.fit(X)
+    print("best k: ", k)
+    Kmeans.plot_fig(X, y_pred)
 
 if __name__ == "__main__":
     main()
